5ed5897fa9cfac7283d0731dcede9e83942a5dc7
Oat-dev2005 <surapich56@gmail.com>
2025-11-13 15:08:35 +0700

    fix2

--- UI_webgame-main/src/app/config/constants.ts
+++ UI_webgame-main/src/app/config/constants.ts
@@ -4,5 +4,5 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Constants {
-  public readonly API_ENDPOINT: string = 'http://192.168.100.128:3000';
+  public readonly API_ENDPOINT: string = 'http://192.168.0.103:3000';
 }
--- UI_webgame-main/src/app/pageadmin/hisdetail/hisdetail.ts
+++ UI_webgame-main/src/app/pageadmin/hisdetail/hisdetail.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderAdmin } from '../../pages/header-admin/header-admin';
-import jsonData from '/web_A/Web-GameTopUp-main/UI_webgame-main/src/assets/user.json'; // เปลี่ยนเป็น relative path
+// import jsonData from '/Web-GameTopUp/UI_webgame-main/src/assets/user.json'; // เปลี่ยนเป็น relative path
 import { CommonModule } from '@angular/common';
 
 @Component({
